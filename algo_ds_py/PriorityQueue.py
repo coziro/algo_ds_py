aa486ea0945d62e7d9b2dcae1ef320b1e0e38233
kojiro <kojiro.nakayama@gmail.com>
2023-11-03 07:54:04 +0900

    Add PriorityQueue

---
+++ algo_ds_py/PriorityQueue.py
@@ -0,0 +1,32 @@
+import heapq
+
+class PriorityQueue:
+
+	def __init__(self, heap=None, max_heap=False):
+		if heap is None:
+			heap = []
+		self.max_heap = max_heap
+		if self.max_heap:
+			heap = [-x for x in heap]
+		self.heap = heap
+		heapq.heapify(self.heap)
+
+	def push(self, item):
+		if self.max_heap:
+			item = -item
+		heapq.heappush(self.heap, item)
+
+	def pop(self):
+		item = heapq.heappop(self.heap)
+		if self.max_heap:
+			item = -item
+		return item
+
+	def heap(self):
+		if self.max_heap:
+			return [-x for x in self.heap]
+		else:
+			return self.heap
+
+	def size(self):
+		return len(self.heap)
