dbc19bb94663b5807e9546eb0544ec2ca6beda67
kojiro <kojiro.nakayama@gmail.com>
2023-11-03 08:44:25 +0900

    Add example for PriorityQueue

---
+++ algo_ds_py/__init__.py
@@ -0,0 +1,1 @@
+from .PriorityQueue import PriorityQueue
---
+++ examples/priority_queue.py
@@ -0,0 +1,29 @@
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+
+from algo_ds_py import PriorityQueue
+
+print('---- min_heap ----')
+pq = PriorityQueue()
+pq.push(10)
+pq.push(30)
+pq.push(20)
+print(pq.pop())
+print(pq.pop())
+print(pq.pop())
+
+print('---- max_heap ----')
+pq = PriorityQueue(max_heap=True)
+pq.push(10)
+pq.push(30)
+pq.push(20)
+print(pq.pop())
+print(pq.pop())
+print(pq.pop())
+
+print('---- heapify ----')
+pq = PriorityQueue([10, 30, 20])
+print(pq.pop())
+print(pq.pop())
+print(pq.pop())
