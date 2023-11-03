a4e71001e36f911f29c6e12ec17a34ca18f9986c
kojiro <kojiro.nakayama@gmail.com>
2023-11-03 16:24:05 +0900

    Add Type Hint

--- algo_ds_py/priority_queue.py
+++ algo_ds_py/priority_queue.py
@@ -1,9 +1,15 @@
 import heapq
+from typing import List, Optional
 
+Item = int | float
 
 class PriorityQueue:
 
-    def __init__(self, heap=None, max_heap=False):
+    def __init__(
+        self,
+        heap: Optional[List[Item]] = None,
+        max_heap: bool = False,
+    ):
         if heap is None:
             self.heap = []
         else:
@@ -15,21 +21,21 @@ class PriorityQueue:
 
         heapq.heapify(self.heap)
 
-    def push(self, item):
+    def push(self, item: Item):
         if self.max_heap:
             item = -item
         heapq.heappush(self.heap, item)
 
-    def pop(self):
+    def pop(self) -> Item:
         item = heapq.heappop(self.heap)
         if self.max_heap:
             item = -item
         return item
 
-    def size(self):
+    def size(self) -> int:
         return len(self.heap)
 
-    def get_heap(self):
+    def get_heap(self) -> List[Item]:
         if self.max_heap:
             return [-x for x in self.heap]
         else:
