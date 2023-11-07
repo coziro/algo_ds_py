798ed64cc5bb771f577ac7bca1192f9e40673112
kojiro <kojiro.nakayama@gmail.com>
2023-11-08 06:27:51 +0900

    Implement Quick Union

--- algo_ds_py/union_find.py
+++ algo_ds_py/union_find.py
@@ -5,16 +5,18 @@ class UnionFind:
 
     def __init__(self, size):
         self.size = size
-        self.root = [i for i in range(self.size)]
+        self.parent = [i for i in range(self.size)]
 
     def find(self, x):
-        return self.root[x]
+        while x != self.parent[x]:
+            x = self.parent[x]
+        return x
 
     def union(self, x, y):
         root_x = self.find(x)
         root_y = self.find(y)
         if root_x != root_y:
-            for i in range(self.size):
-                if self.root[i] == root_y:
-                    self.root[i] = root_x
+            self.parent[root_y] = root_x
 
+    def same(self, x, y):
+        return self.find(x) == self.find(y)
