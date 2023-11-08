f15c6ab207c11021b327fdbbcba38eb9048151a9
kojiro <kojiro.nakayama@gmail.com>
2023-11-09 06:33:36 +0900

    Implement union by rank

--- algo_ds_py/union_find.py
+++ algo_ds_py/union_find.py
@@ -6,6 +6,7 @@ class UnionFind:
     def __init__(self, size):
         self.size = size
         self.parent = [i for i in range(self.size)]
+        self.rank = [1] * self.size
 
     def find(self, x):
         while x != self.parent[x]:
@@ -16,7 +17,13 @@ class UnionFind:
         root_x = self.find(x)
         root_y = self.find(y)
         if root_x != root_y:
-            self.parent[root_y] = root_x
+            if self.rank[root_x] > self.rank[root_y]:
+                self.parent[root_y] = root_x
+            elif self.rank[root_x] < self.rank[root_y]:
+                self.parent[root_x] = root_y
+            else:
+                self.parent[root_y] = root_x
+                self.rank[root_x] += 1
 
     def same(self, x, y):
         return self.find(x) == self.find(y)
