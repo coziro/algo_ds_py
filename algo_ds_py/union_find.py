d18112c35d595bd4cdc25f3f84f2c02f761a0dd4
kojiro <kojiro.nakayama@gmail.com>
2023-11-10 02:44:51 +0900

    Implement path compression optimization

--- algo_ds_py/union_find.py
+++ algo_ds_py/union_find.py
@@ -14,9 +14,10 @@ class UnionFind:
         self.rank = [1] * self.size
 
     def find(self, x):
-        while x != self.parent[x]:
-            x = self.parent[x]
-        return x
+        if x == self.parent[x]:
+            return x
+        self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
 
     def union(self, x, y):
         root_x = self.find(x)
