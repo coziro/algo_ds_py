4b162efb61f4c69c078d186d1b79d0ca7c405c57
kojiro <kojiro.nakayama@gmail.com>
2023-11-11 20:10:54 +0900

    Add example for UnionFind

---
+++ examples/example_union_find.py
@@ -0,0 +1,17 @@
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+
+from algo_ds_py import UnionFind  # noqa: E402
+
+uf = UnionFind(3)
+uf.union(1, 2)
+
+print(f'root of 0: {uf.find(0)}')
+print(f'root of 1: {uf.find(1)}')
+print(f'root of 2: {uf.find(2)}')
+
+print(f'roots: {uf.roots()}')
+print(f'group_count: {uf.group_count()}')
+print(f'all_group_members: {uf.all_group_members()}')
