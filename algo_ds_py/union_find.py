055c73dbdaea232fe4e613608b2e5477c5de7882
kojiro <kojiro.nakayama@gmail.com>
2023-11-11 20:09:04 +0900

    Refactoring

--- algo_ds_py/union_find.py
+++ algo_ds_py/union_find.py
@@ -1,5 +1,6 @@
-from typing import List
 from collections import defaultdict
+from typing import List
+
 
 class UnionFind:
     """A union find (disjoint set) implementation.
--- tests/test_union_find.py
+++ tests/test_union_find.py
@@ -1,13 +1,5 @@
 from algo_ds_py import UnionFind
 
-"""
-def test_union_and_find():
-    uf = UnionFind(3)
-    uf.union(1, 2)
-    assert uf.find(0) == 0
-    assert uf.find(1) == 1
-    assert uf.find(2) == 1
-"""
 
 def test_union_find():
     uf = UnionFind(3)
