7bb1a71c1348df82197340f779244e2b0f8451a8
kojiro <kojiro.nakayama@gmail.com>
2024-02-04 11:29:08 +0900

    update logger

--- algo_ds_py/logger.py
+++ algo_ds_py/logger.py
@@ -5,29 +5,39 @@ logger = Logger(print=True)
 logger.print_global_vars(header='input')
 logger.print_vars(locals())
 """
+from typing import Any, Optional
+
 
 class Logger:
 
-    def __init__(self, print: bool=False) -> None:
-        self.print = print
-
-    def print_header(self, title: str) -> None:
-        if not self.print:
-            return
-        print(f'---- {title} ----')
-
-    def print_subheader(self, title: str) -> None:
-        if not self.print: 
-            return
-        print(f'-- {title} --')
-
-    def print_var(
+    def __init__(
         self,
-        symbol,
-        value,
-        pprint:bool = False
+        print: bool=False,
+        pprint: bool=False,
+    ) -> None:
+        self.print = print
+        self.ppring = pprint
+
+    def print_header(
+        self,
+        title: str
+    ) -> None:
+        if self.print:
+            print(f'---- {title} ----')
+
+    def print_subheader(
+        self,
+        title: str
+    ) -> None:
+        if print:
+            print(f'-- {title} --')
+
+    def _print_single_var_info(
+        self,
+        symbol: str,
+        value: Any,
     ):
-        if pprint and isinstance(value, list):
+        if self.pprint and isinstance(value, list):
             print(f'{symbol} ({type(value).__name__}):')
             for i, elem in enumerate(value):
                 print(f'  {i}: {elem}')
@@ -35,32 +45,29 @@ class Logger:
             print(f'{symbol}({type(value).__name__}): {value}')
 
     def print_vars(
-        self, 
-        vars: dict,
-        pprint: bool = False,
-        var_names: list = None,
+        self,
+        var_info: dict,
+        var_names: Optional[list] = None,
     ):
         if not self.print:
             return
 
         if var_names:
             for symbol in var_names:
-                value = vars[symbol]
-                self.print_var(symbol, value, pprint)
+                value = var_info[symbol]
+                self._print_single_var_info(symbol, value)
 
         else:
-            for symbol, value in vars.items():
+            for symbol, value in var_info.items():
                 if symbol.startswith('_'):
                     continue
                 if callable(value):
                     continue
-                else:
-                    self.print_var(symbol, value, pprint)
+                self._print_single_var_info(symbol, value)
 
     def print_global_vars(
-        self, 
-        header:str = None,
-        pprint:bool = False,
+        self,
+        header: Optional[str] = None,
     ) -> None:
         if not self.print:
             return
@@ -68,7 +75,7 @@ class Logger:
         if header:
             self.print_header(header)
 
-        self.print_vars(globals(), pprint=pprint)
+        self.print_vars(globals())
 
     def __repr__(self) -> str:
         return f'Logger (print={self.print})'
